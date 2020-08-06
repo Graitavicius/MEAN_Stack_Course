603d6f607284c02512d31c03f16dc6cd2295253b
Graitavicius <grait.mindaugas@gmail.com>
2020-08-06 16:42:44 +0300

    outputing posts, creating posts

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -6,13 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent implements OnInit {
-  enteredValue = '';
-  newPost = 'NO CONTENT';
+  enteredTitle = '';
+  enteredContent = '';
+  @Output() postCreated = new EventEmitter();
+
   constructor() {}
 
   ngOnInit(): void {}
 
   onAddPost() {
-    this.newPost = this.enteredValue;
+    const post = { title: this.enteredContent, content: this.enteredContent };
+    this.postCreated.emit(post);
   }
 }

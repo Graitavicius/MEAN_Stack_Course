b49dfbc30b55b1310224f6dbf707c4d13370a048
Graitavicius <grait.mindaugas@gmail.com>
2020-08-08 22:19:48 +0300

    added functionality to edit button, updating posts on the server

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -31,6 +31,12 @@ export class PostsService {
       });
   }
 
+  getPost(id: string) {
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      'http://localhost:3000/api/posts/' + id
+    );
+  }
+
   getPostUpdateListener() {
     return this.postsUpdated.asObservable();
   }
@@ -46,6 +52,19 @@ export class PostsService {
         const postId = resData.postId;
         post.id = postId;
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http
+      .put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe((response) => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex((p) => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
